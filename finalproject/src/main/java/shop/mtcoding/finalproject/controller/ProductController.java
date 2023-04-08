7994f5a62f464f7b7a3b187fc4567bffab989130
DH <shim5013@naver.com>
2023-04-08 19:54:04 +0900

    초기세팅완료

---
+++ finalproject/src/main/java/shop/mtcoding/finalproject/controller/ProductController.java
@@ -0,0 +1,21 @@
+package shop.mtcoding.finalproject.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.finalproject.model.Product;
+import shop.mtcoding.finalproject.model.ProductRepository;
+
+@Controller
+public class ProductController {
+
+   
+}
