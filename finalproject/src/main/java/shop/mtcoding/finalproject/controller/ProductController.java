56df01bb5f98da4c01ad3bb5db6221045d7da263
DH <shim5013@naver.com>
2023-04-09 01:38:48 +0900

    완료

--- finalproject/src/main/java/shop/mtcoding/finalproject/controller/ProductController.java
+++ finalproject/src/main/java/shop/mtcoding/finalproject/controller/ProductController.java
@@ -2,8 +2,6 @@ package shop.mtcoding.finalproject.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,5 +15,63 @@ import shop.mtcoding.finalproject.model.ProductRepository;
 @Controller
 public class ProductController {
 
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping({ "/", "/product" })
+    public String findAll(Model model) { // model = request
+        List<Product> productList = productRepository.findAll();
+        model.addAttribute("productList", productList);
+        return "product/main"; // request 새로 만들어짐 - 덮어쒸움(프레임워크)
+    }
+
+    @GetMapping("/product/{id}")
+    public String findOne(@PathVariable int id, Model model) {
+        Product product = productRepository.findById(id);
+        model.addAttribute("product", product);
+        return "product/detail";
+    }
+
+    @GetMapping("/product/addForm")
+    public String addForm() {
+        return "product/addForm";
+    }
+
+    @PostMapping("/product/add")
+    public String add(String name, Integer price, Integer qty) {
+        int result = productRepository.insert(name, price, qty);
+        if (result == 1) {
+            return "redirect:/product";
+        } else {
+            return "redirect:product/addForm";
+        }
+    }
+
+    @PostMapping("/product/{id}/delete")
+    public String delete(@PathVariable int id) {
+        int result = productRepository.delete(id);
+        if (result == 1) {
+            return "redirect:/";
+        } else {
+            return "redirect:/product/" + id;
+        }
+    }
+
+    @GetMapping("product/{id}/updateForm")
+    public String updateForm(@PathVariable int id, Model model) {
+        Product product = productRepository.findById(id);
+        model.addAttribute("product", product);
+        return "product/updateForm";
+    }
+
+    @PostMapping("/product/{id}/update")
+    public String update(@PathVariable int id, String name, int price, int qty) {
+        int result = productRepository.update(id, name, price, qty);
+        if (result == 1) {
+            return "redirect:/product/" + id;
+        } else {
+            return "redirect:/product/" + id + "/updateForm";
+        }
+    }
    
 }
